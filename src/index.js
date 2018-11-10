ee79ebddadd9e91af3e8d730ad74c92f95d7fe4b
Omar Hafez <hafezoa@gmail.com>
2018-11-10 16:33:10 -0600

    grammar

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ theater
     .addActor('visitor')
     .addActor('header')
     .addScene('header:Journal entry no. 2322', 2000)
-    .addScene('me:Life suddenly feels like a video game simulation. The desire to level up and acquire new skills has started to fade. There is little left out to do but to create things that help and inspire others.', 1600)
+    .addScene('me:Life suddenly feels like a video game simulation. Albeit, the desire to level up and acquire new skills has started to fade. There is little left to do but to create things that help and inspire others.', 1600)
     .addActor('me2')
     .addScene('me2: Big things have small beginnings.')
 
